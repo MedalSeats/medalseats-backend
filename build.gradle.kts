2209fda2b2c32448376b27ccd3efd9782238f172
Gustavo Gitzel <gustavo.gitzel@ifood.com.br>
2024-06-12 16:07:59 -0300

    deploy to heroku

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,13 @@ val kotlinVersion: String by project
 val jvmVersion: String by project
 val Project.fullName: String get() = (parent?.fullName?.plus("-") ?: "") + name
 
+
 tasks {
+	compileJava {
+		sourceCompatibility = "1.8"
+		targetCompatibility = "1.8"
+	}
+
 	register<JacoboTask>("jacobo") {
 		jacocoReport = file("$buildDir/reports/jacoco/jacocoAggregatedReport/jacocoAggregatedReport.xml")
 		coberturaReport = file("$buildDir/reports/cobertura/cobertura.xml")
